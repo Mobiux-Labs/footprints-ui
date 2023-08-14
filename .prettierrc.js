09aa3c265ee278291d1aa36d8c18045dd2ff7373
Swapnil Kaushik <swapnil@mobiux.in>
2023-08-14 16:39:01 +0530

    Initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  semi: true,
+  tabWidth: 2,
+  printWidth: 80,
+  useTabs: false,
+  arrowParens: "avoid",
+  bracketSpacing: true,
+  trailingComma: "none",
+  singleQuote: true,
+  endOfLine: "lf" // Set to "lf" for Unix-style line endings
+};
