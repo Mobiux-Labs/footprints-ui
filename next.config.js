519e8ea031c9494c07b0a0c80d8c23db09ea9e8b
Swapnil Kaushik <swapnil@mobiux.in>
2023-08-17 16:47:19 +0530

    quick fixes

--- next.config.js
+++ next.config.js
@@ -1,20 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: 'build',
-  module: {
-    rules: [
-      {
-        test: /\.js$/, // Match JavaScript files
-        exclude: /node_modules/, // Exclude external modules
-        use: {
-          loader: 'babel-loader', // Use Babel loader
-          options: {
-            presets: ['@babel/preset-env'] // Babel preset for environment
-          }
-        }
-      }
-    ]
-  }
+  distDir: 'build'
 };
 
 module.exports = nextConfig;
