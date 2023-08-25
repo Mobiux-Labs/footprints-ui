53fe752e45f475fcc21321531d4c5e0c9558109e
Swapnil Kaushik <swapnil@mobiux.in>
2023-08-25 14:22:42 +0530

    fix: 🐛 added new pkgs and new lint rules

--- app/page.js
+++ app/page.js
@@ -1,3 +1,25 @@
+'use client';
+
+import Form from '@/components/Form';
+
 export default function Home() {
-  return null;
+  const onSubmitHandler = () => {};
+
+  return (
+    <Form
+      onSubmitHandler={e => onSubmitHandler(e)}
+      items={{
+        label: 'Name',
+        errorText: 'This field is required',
+        inputProps: {
+          type: 'text',
+          // value: values.name,
+          // onChange: e => onChangeHandler('name', e.target.value),
+          placeholder: 'Name',
+          required: true
+        },
+        bodyClassName: 'col-span-2'
+      }}
+    />
+  );
 }
