6b7ad9df0228b5f357d9e7034baf0308d9a804d1
Anshuman Saagar <anshuman@mobiux.in>
2023-10-12 12:06:07 +0530

    Add config files

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { resolve } from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, "lib/main.js"),
+      name: "mobiuxui",
+      fileName: "mobiuxui"
+    },
+    rollupOptions: {
+      external: ["react"],
+      output: {
+        globals: {
+          react: "React"
+        }
+      }
+    }
+  },
   plugins: [react()],
 })
